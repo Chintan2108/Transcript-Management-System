1e4a2229d6afdf74cf9e4cec7c0bc0e804699353
Devesh Hatkar <deveshhatkar@gmail.com>
2018-03-11 11:23:04 +0530

    updated mailer and confirmation links

--- server-scripts/student-registration-onconfirm.php
+++ server-scripts/student-registration-onconfirm.php
@@ -1,11 +1,7 @@
 <?php
-<<<<<<< HEAD
-=======
 
 
-	$email = $_GET["email"];
 
->>>>>>> e50b52772554bc51fd04f04af831a3fb6b80dd78
 	$email = $_GET['email'];
 
 	$conn = mysqli_connect("localhost", "root", "","tms");
@@ -18,66 +14,6 @@
 
 	$result = $conn->query($query);
 
-<<<<<<< HEAD
-=======
-
-	if ($result->num_rows > 0) 
-	{
->>>>>>> e50b52772554bc51fd04f04af831a3fb6b80dd78
-
-	if ($result->num_rows >  0) 
-	{
-
-<<<<<<< HEAD
-/*	   	while($row = mysql_fetch_assoc($result)){
-	          
-=======
-	$university = $row['university_id'];
-	$institute=$row['institute'];
-	$programme=$row['programme'];
-	$join_year=$row['joining_year'];
-	$grad_year=$row['graduation_year'];
-	$student_id=$row['id'];
-	$fname=$row['first_name'];
-	$lname=$row['last_name'];
-	$contact_no=$row['contact_no'];
-	$flat_no=$row['flat_no'];
-	$building_name=$row['building_name'];
-	$street_no=$row['street_no'];
-	$street_name=$row['street_name'];
-	$city=$row['city'];
-	$p_code=$row['postal_code'];
-	$state=$row['state'];
-	$email=$row['email'];
-	$password = $row['password'];
-	}
-	echo $institute;
-
-	if ($result->num_rows > 0) 
-	{ 
-		$row = $result -> fetch_assoc();
->>>>>>> e50b52772554bc51fd04f04af831a3fb6b80dd78
-
-		$university = $row['university_id'];
-		$institute=$row['institute'];
-		$programme=$row['programme'];
-		$join_year=$row['joining_year'];
-		$grad_year=$row['graduation_year'];
-		$student_id=$row['id'];
-		$fname=$row['first_name'];
-		$lname=$row['last_name'];
-		$contact_no=$row['contact_no'];
-		$flat_no=$row['flat_no'];
-		$building_name=$row['building_name'];
-		$street_no=$row['street_no'];
-		$street_name=$row['street_name'];
-		$city=$row['city'];
-		$p_code=$row['postal_code'];
-		$state=$row['state'];
-		$email=$row['email'];
-		$password = $row['password'];
-		}
-*/		
 
 		if ($result->num_rows > 0) 
 		{ 
@@ -105,19 +41,13 @@
 			$query1 = "INSERT INTO students (id, first_name, last_name, email, university_id, institute, programme, flat_no, building_name, street_no, street_name, city, postal_code, state, password, joining_year, graduation_year, contact_no) VALUES('$student_id', '$fname', '$lname', '$email', '$university', '$institute', '$programme', '$flat_no', '$building_name', '$street_no', '$street_name', '$city', '$p_code', '$state', '$password', '$join_year', '$grad_year', '$contact_no')";
 	 		$conn -> query($query1);
 		}
-	}
+	
 
 
 
 	$sql = "DELETE FROM temp_students WHERE email='$email'";
 	$conn -> query($sql);
 
-<<<<<<< HEAD
+
 	header("Location: ../../student/registration-student-confirmed.html");
-=======
-	header("Location: ../../student/registration-student-confirmed.html")
-
-?>
->>>>>>> e50b52772554bc51fd04f04af831a3fb6b80dd78
-
 ?>
--- server-scripts/student-registration-onsubmit.php
+++ server-scripts/student-registration-onsubmit.php
@@ -31,42 +31,50 @@
 	$q = "SELECT email FROM gtu WHERE student_id='$student_id'";
 	$r = $conn -> query($q);
 
+	$email_add = "helloo";
+
 	if($r -> num_rows > 0)
 	{
 		$row = $r -> fetch_assoc();
 		$email_add = $row['email'];
-	}
 
+		$mail = new PHPMailer(); // create a new object
+		$mail->IsSMTP(); // enable SMTP
+		//$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+		$mail->SMTPAuth = true; // authentication enabled
+		$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+		$mail->Host = "smtp.googlemail.com";
+		$mail->Port = 587; // or 587
+		$mail->IsHTML(true);
+		$mail->Username = "transcriptsmanager@gmail.com";
+		$mail->Password = "transcript1234";
+		$mail->SetFrom("deveshhatkar@gmail.com");
+		$mail->AddAddress($email_add);   // Add a recipient
+
+
+		$mail->Subject = 'Confirmation Link | Transcript';
+		$mail->Body    = "http://localhost//git//Transcript-Management-System//server-scripts//student-registration-onconfirm.php?email=$email";
+		
+		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+		if($mail->send()) {
+		 	$query = "INSERT INTO temp_students (id, first_name, last_name, email, university_id, institute, programme, flat_no, building_name, street_no, street_name, city, postal_code, state, password, joining_year, graduation_year, contact_no) VALUES('$student_id', '$fname', '$lname', '$email', '$university', '$institute', '$programme', '$flat_no', '$building_name', '$street_no', '$street_name', '$city', '$p_code', '$state', '$password', '$join_year', '$grad_year', '$contact_no')";
+		 	$conn -> query($query);
+		 	header("Location: ../student/registration-student-ack.html");
+		} 
+		else
+		{
+			echo "Error!";
+		}
+		
+	}
+	else{
+		echo "Invalid enrollment no. You must be a student of the university!";
+	}
 	echo $email_add;
 	
-	$mail = new PHPMailer(); // create a new object
-	$mail->IsSMTP(); // enable SMTP
-	$mail->SMTPDebug = 3; // debugging: 1 = errors and messages, 2 = messages only
-	$mail->SMTPAuth = true; // authentication enabled
-	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-	$mail->Host = "smtp.googlemail.com";
-	$mail->Port = 465; // or 587
-	$mail->IsHTML(true);
-	$mail->Username = "transcriptsmanager@gmail.com";
-	$mail->Password = "transcript1234";
-	//$mail->SetFrom("deveshhatkar@gmail.com");
-	$mail->AddAddress($email_add);   // Add a recipient
-
-
-	$mail->Subject = 'Confirmation Link | Transcript';
-	$mail->Body    = "http://localhost//git//Transcript-Management-System//server-scripts//student-registration-onconfirm.php?email=$email";
 	
-	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-	if($mail->send()) {
-	 	$query = "INSERT INTO temp_students (id, first_name, last_name, email, university_id, institute, programme, flat_no, building_name, street_no, street_name, city, postal_code, state, password, joining_year, graduation_year, contact_no) VALUES('$student_id', '$fname', '$lname', '$email', '$university', '$institute', '$programme', '$flat_no', '$building_name', '$street_no', '$street_name', '$city', '$p_code', '$state', '$password', '$join_year', '$grad_year', '$contact_no')";
-	 	$conn -> query($query);
-	 	header("Location: ../student/registration-student-ack.html");
-	} 
-	else
-	{
-		echo "Error!";
-	}
+	
 
 	
 	
